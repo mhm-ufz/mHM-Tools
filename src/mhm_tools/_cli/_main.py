63126e4094ec48802b3b223c8587a57461621977
Sebastian Müller <mueller.seb@posteo.de>
2023-03-28 13:49:53 +0200

    docs: add CLI documentation

--- src/mhm_tools/_cli/_main.py
+++ src/mhm_tools/_cli/_main.py
@@ -5,19 +5,7 @@ from .. import __version__
 from . import _bankfull
 
 
-def main(argv=None):
-    """
-    Main CLI routine.
-
-    Parameters
-    ----------
-    argv : list of str
-        command line arguments, default is None
-
-    Returns
-    -------
-        result of the called sub-argument routine
-    """
+def _get_parser():
     parent_parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -35,9 +23,28 @@ def main(argv=None):
         title="subcommands", dest="command", required=True
     )
 
-    parser = subparsers.add_parser("bankfull", help=_bankfull.__doc__)
+    # all sub-parsers should be added here
+
+    parser = subparsers.add_parser("bankfull", description=_bankfull.__doc__)
     _bankfull.add_args(parser)
     parser.set_defaults(func=_bankfull.bankfull)
 
-    args = parent_parser.parse_args(argv)
+    # return the parser
+    return parent_parser
+
+
+def main(argv=None):
+    """
+    Main CLI routine.
+
+    Parameters
+    ----------
+    argv : list of str
+        command line arguments, default is None
+
+    Returns
+    -------
+        result of the called sub-argument routine
+    """
+    args = _get_parser().parse_args(argv)
     return args.func(args)
