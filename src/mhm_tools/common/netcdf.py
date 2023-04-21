3378164a1037d9b4513842525509f02b253b8f84
Sebastian Müller <mueller.seb@posteo.de>
2023-04-21 17:21:57 +0200

    netcdf: add bounds adding routine

--- src/mhm_tools/common/netcdf.py
+++ src/mhm_tools/common/netcdf.py
@@ -1,4 +1,6 @@
-"""Common NetCDF routines."""
+"""Common NetCDF/xarray routines."""
+import xarray as xr
+
 from .constants import NC_ENCODE_DEFAULTS
 
 
@@ -15,7 +17,6 @@ def set_netcdf_encoding(ds, var_encoding=None):
         by default :any:`NC_ENCODE_DEFAULTS`
     """
     var_encoding = var_encoding or NC_ENCODE_DEFAULTS
-    # no FillValue for dim-coords and bounds
     dims = set(ds.dims)
     all_coords = set(ds.coords)
     dim_coords = all_coords & dims  # intersection
@@ -26,5 +27,33 @@ def set_netcdf_encoding(ds, var_encoding=None):
     for v in aux_coords | vars:  # union
         ds[v].encoding = var_encoding
 
+    # missing data not allowed for coordinates by CF convention
+    # http://cfconventions.org/cf-conventions/cf-conventions.html#missing-data
+    # no FillValue for dim-coords and bounds
     for v in dim_coords | bnds:  # union
         ds[v].encoding = {"_FillValue": None}
+
+
+def generate_bounds(da, bounds_dim="bnds"):
+    """
+    Generate bounds for a given coordinate.
+
+    Parameters
+    ----------
+    da : :class:`xarray.Dataarray`
+        Data array holding a coordinate.
+
+    Returns
+    -------
+    :class:`xarray.Dataarray`
+        Bounds for the given coordinate.
+    """
+    (dim,) = da.dims
+    diff = da.diff(dim)
+    lower = da - diff / 2
+    upper = da + diff / 2
+    bounds = xr.concat([lower, upper], dim=bounds_dim)
+    first = (bounds.isel({dim: 0}) - diff.isel({dim: 0})).assign_coords(
+        {dim: da[dim][0]}
+    )
+    return xr.concat([first, bounds], dim=dim).transpose()
