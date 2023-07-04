95546b81f2ed4a1bc4921798acf4c6cb60c84228
luedke <valentin-simon.luedke@ufz.de>
2023-07-04 15:29:09 +0200

    Add test for timeseries with nan values

--- tests/test_hydrograph.py
+++ tests/test_hydrograph.py
@@ -13,31 +13,38 @@ class TestHydrograph(unittest.TestCase):
     def setUp(self):
         self.in_file = HERE / "output" / "discharge.nc"
         
-    def test_calc_kge(self):
+    def test_calc_objectives(self):
         hydro = Hydrograph('error')
         q_test = np.random.random(1000)
 
         # test equal arrays
-        kge = hydro.calc_kling_gupta_efficiency(q_test, q_test)
+        nse, kge = hydro.calc_objectives(q_test, q_test)
         self.assertTrue(np.abs(kge['KGE']-1) < 1e-4)
+        self.assertTrue(np.abs(nse-1) < 1e-4)
 
         # test offset
-        kge = hydro.calc_kling_gupta_efficiency(q_test, q_test+1)
+        nse, kge = hydro.calc_objectives(q_test, q_test+1)
         self.assertTrue(np.abs(kge['KGE']-1) > 1e-4)
+        self.assertTrue(np.abs(nse-1) > 1e-4)
 
-        kge = hydro.calc_kling_gupta_efficiency(np.random.normal(2,0.5,1000000), np.random.normal(1,1,1000000))
-        print(kge)
+        nse, kge = hydro.calc_objectives(np.random.normal(2,0.5,1000000), np.random.normal(1,1,1000000))
         self.assertTrue(np.abs(kge['alpha']-2) < 1e-1) # test if the relation of standard diviations is correct
         self.assertTrue(np.abs(kge['beta']-0.5) < 1e-1) # test if the relation of the mean is correct
 
-        kge = hydro.calc_kling_gupta_efficiency(np.linspace(0,10,100), np.linspace(1,11,100))
+        nse, kge = hydro.calc_objectives(np.linspace(0,10,100), np.linspace(1,11,100))
         self.assertTrue(np.abs(kge['r']-1) < 1e-4) # slope
         self.assertTrue(np.abs(kge['offset']-1) < 1e-4) # offset
 
         # test for wrong input
-        # self.assertRaises(TypeError, hydro.calc_kling_gupta_efficiency(np.random.normal(2,0.5,10000), np.random.normal(1,1,1000000)))
-        
-
+        with self.assertRaises(Exception) as context:
+            hydro.calc_objectives(np.random.normal(2,0.5,1), np.random.normal(1,1,10))
+        self.assertTrue('The two timeseries do not have the same length.', str(context.exception))
+                
+        # test if nan and None values are removed correctly
+        q_test_2 = np.array([1,2,3,None,5,6,np.nan,8])
+        nse, kge = hydro.calc_objectives(q_test_2, np.arange(1,9))
+        self.assertTrue(nse - 1 < 1e-6)
+        self.assertTrue(kge['KGE'] - 1 < 1e-6)
 
 if __name__ == "__main__":
     unittest.main()
