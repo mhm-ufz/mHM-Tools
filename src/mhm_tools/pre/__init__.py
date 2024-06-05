8959da871a62e6a9a90a8f814426ed4433556553
luedke <valentin-simon.luedke@ufz.de>
2024-06-05 18:25:47 +0200

    Fixed subdomain_masks init

--- src/mhm_tools/pre/__init__.py
+++ src/mhm_tools/pre/__init__.py
@@ -12,6 +12,7 @@ latlon file creation
 
 from . import catchment, latlon
 from .catchment import create_catchment, merge_catchment
+from .subdomain_masks import create_subdomain_masks
 from .latlon import create_latlon, xy_to_latlon
 
 __all__ = ["latlon"]
