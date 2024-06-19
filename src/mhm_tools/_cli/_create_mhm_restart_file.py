51408c082ae8debe28191622abf77227d42d8685
luedke <valentin-simon.luedke@ufz.de>
2024-06-19 12:54:48 +0200

    remove last linter complaint

--- src/mhm_tools/_cli/_create_mhm_restart_file.py
+++ src/mhm_tools/_cli/_create_mhm_restart_file.py
@@ -85,6 +85,6 @@ def run(args):
         lat_max_target_grid=lat_max_target_grid,
         l0_resolution=l0_resolution,
         l1_resolution=l1_resolution,
-        l1_increment=args.l1_increment
+        l1_increment=args.l1_increment,
     )
     restart_creator.create_restart_file()
