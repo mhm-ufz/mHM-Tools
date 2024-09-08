8ded3d4a8e26210cc635bae507f4241e47ec07d2
luedke <valentin-simon.luedke@ufz.de>
2024-09-08 20:29:19 +0200

    Try something

--- src/mhm_tools/pre/catchment.py
+++ src/mhm_tools/pre/catchment.py
@@ -305,10 +305,11 @@ class Catchment:
         logger.debug(f"min_row: {min_row}, max_row: {max_row}, min_col: {min_col}, max_col: {max_col}")
         logger.debug(f"shape of catchment_mask: {self.catchment_mask.shape}")
         logger.debug(f"shape of lat and lon: {self.ds.lat.shape}, {self.ds.lon.values.shape}")
-        logger.debug(f"lat: min: {np.min(self.ds.lat.values)}, max: {np.max(self.ds.lat.values)} - {self.ds.lat.values[min_row]}, {self.ds.lat.values[max_row]}")
+        logger.debug(f"lat: min: {np.min(self.ds.lat.values)}, max: {np.max(self.ds.lat.values)} - {self.ds.lat.values[-min_row]}, {self.ds.lat.values[-max_row]}")
         logger.debug(f"lon: min: {np.min(self.ds.lon.values)}, max: {np.max(self.ds.lon.values)} - {self.ds.lon.values[min_col]}, {self.ds.lon.values[max_col]}")
-        lat_slice = slice(self.ds.lat.values[-min_row], self.ds.lat.values[-max_row])
-        lon_slice = slice(self.ds.lon.values[min_col], self.ds.lon.values[max_col])
+        lat_slice = slice(self.ds.lat[-min_row], self.ds.lat[-max_row])
+        lon_slice = slice(self.ds.lon[min_col], self.ds.lon[max_col])
+
         return lat_slice, lon_slice
 
 # use this code to merge the rolled and non-rolled file
