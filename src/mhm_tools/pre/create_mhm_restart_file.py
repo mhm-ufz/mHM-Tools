828c55f0048f3ff7db21d264ea752fa559d64693
luedke <valentin-simon.luedke@ufz.de>
2024-10-18 16:43:04 +0200

    Additional debug logging

--- src/mhm_tools/pre/create_mhm_restart_file.py
+++ src/mhm_tools/pre/create_mhm_restart_file.py
@@ -799,8 +799,10 @@ class MHMRestartFile:
     def _correct_restart_file(self, ds):
         ds_mask = xr.open_dataset(self.grid.land_mask_file)
         ds_mask = ds_mask.sel(lon=slice(self.grid.l1.lon_min, self.grid.l1.lon_max), lat=slice(self.grid.l1.lat_max, self.grid.l1.lat_min))
-        logger.debug(f'lon: ds {ds['lon_out'][0]}-{ds['lon_out'][-1]} ; grid {self.grid.l1.lon_min}-{self.grid.l1.lon_max}')
-        logger.debug(f'lat: ds {ds['lat_out'][0]}-{ds['lat_out'][-1]} ; grid {self.grid.l1.lat_min}-{self.grid.l1.lat_max}')
+        logger.debug(f'lon: ds {ds['lon_out'].values[0]}-{ds['lon_out'].values[-1]} ; grid {self.grid.l1.lon_min}-{self.grid.l1.lon_max}')
+        logger.debug(f'lat: ds {ds['lat_out'].values[0]}-{ds['lat_out'].values[-1]} ; grid {self.grid.l1.lat_min}-{self.grid.l1.lat_max}')
+        logger.debug(f'mask: {np.shape(ds_mask["land_mask"].data)}')
+        logger.debug(f'ds: {np.shape(ds["L1_SoilMoistureExponent"].data)}')
         try: 
             ds_mask = ds_mask.sortby("latitude")
         except Exception as e:
