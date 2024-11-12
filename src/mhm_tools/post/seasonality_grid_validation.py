83b8d8104dd4b5f8507a109367d82af7ee89aab1
luedke <valentin-simon.luedke@ufz.de>
2024-11-12 18:42:25 +0100

    Return key if #dims is not 1

--- src/mhm_tools/post/seasonality_grid_validation.py
+++ src/mhm_tools/post/seasonality_grid_validation.py
@@ -88,7 +88,9 @@ def get_coord_key(ds, lat=False, lon=False):
     for key in keys:
         if key in ds and len(ds[key].shape) == 1:
             return key
-    logger.debug(ds)
+        elif key in ds: 
+            logger.warning(f"{type(ds)} contains key: {key} but ds[key] has shape {ds[key].shape}.")
+            return key
     raise ValueError(f"None of {keys} in dataset.")
 
 def get_coord_values(ds, lat=False, lon=False):
