d9519a54a421368d8120a129067ff325634090c4
luedke <valentin-simon.luedke@ufz.de>
2025-01-07 12:46:25 +0100

    Fix Domain dimension

--- src/mhm_tools/pre/subdomain_masks.py
+++ src/mhm_tools/pre/subdomain_masks.py
@@ -230,12 +230,12 @@ class CreateSubdomainMasks:
     def use_land_mask(self, lat, lon):
         """Reencode and mask the input files"""
         logger.info('Non global file selected. Only reencoding and masking the input.')
-
-        logger.debug(f"lat={slice(lat[0], lat[-1])}")
-        logger.debug(f"lon={slice(lon[0], lon[-1])}")
+        res = lon[1]-lon[0]
+        logger.debug(f"lat={slice(lat.values[0], lat.values[-1])}")
+        logger.debug(f"lon={slice(lon.values[0], lon.values[-1])}")
         # Read and slice the land mask
         land_mask = self.read_var(fname=self.land_file, var_name="land_mask").astype(bool)
-        land_mask = land_mask.sel(lat=slice(lat[0], lat[-1]), lon=slice(lon[0], lon[-1]))
+        land_mask = land_mask.sel(lat=slice(lat[0], lat[-1]-res), lon=slice(lon[0]-res, lon[-1]))
 
         # Read and slice the reference file based on the land mask coordinates
         logger.info(f"Reading {self.ref_file}")
@@ -252,8 +252,35 @@ class CreateSubdomainMasks:
         ds_sub_ref_file = ds_ref_file.copy()
         for data_var in ds_sub_ref_file.data_vars:
             logger.info(f"processing {data_var}")
-            ds_sub_ref_file[data_var].values[np.isnan(land_mask)] = np.nan
+            data_var_values = ds_sub_ref_file[data_var].values
+            land_mask_values = land_mask.values
 
+            if data_var == 'fdir':
+                # nan values inside the land mask are changed to sink values
+                # ds_sub_ref_file[data_var] = ds_sub_ref_file[data_var].where(
+                #     ~np.isnan(ds_sub_ref_file[data_var]) | np.isnan(land_mask),
+                #     0
+                # )
+                # # replace all values where land mask is nan with nan unless they are sink values
+                # ds_sub_ref_file[data_var] = ds_sub_ref_file[data_var].where(
+                #     ~np.isnan(land_mask) | (ds_sub_ref_file[data_var]==0),
+                #     np.nan
+                # )
+                # Replace values where ds_sub_ref_file[data_var] is NaN and land_mask is not NaN with 0
+
+                # First condition: Set to 0 where data_var is NaN and land_mask is not NaN
+                data_var_values[np.isnan(data_var_values) & ~np.isnan(land_mask_values)] = 0
+
+                # Second condition: Replace all values where land_mask is NaN with NaN unless they are sink values (0)
+                data_var_values[np.isnan(land_mask_values) & (data_var_values != 0)] = np.nan
+
+            else:
+                # replace all values where land mask is nan with nan
+                # ds_sub_ref_file[data_var] = ds_sub_ref_file[data_var].where(~np.isnan(land_mask), np.nan)
+                data_var_values[np.isnan(land_mask_values)] = np.nan
+            # Assign the modified values back to the DataArray
+            ds_sub_ref_file[data_var].values = data_var_values
+           
         # Write the output to a netCDF file
         fname = self.out_file_name + ".nc"
         logger.info(f"Writing to {fname}")
