a861164690e597b2fc7353d36e1f441506f33aba
luedke <valentin-simon.luedke@ufz.de>
2025-11-01 13:17:16 +0100

    Fix that hydrograph can take discharge file directly and not only path

--- src/mhm_tools/post/hydrograph.py
+++ src/mhm_tools/post/hydrograph.py
@@ -422,7 +422,10 @@ class Hydrograph:
             TypeError: If the variable name in the discharge dataset is not a string.
         """
         path = Path(path)
-        discharge_file = path / "discharge.nc"
+        if path.is_dir():
+            discharge_file = path / "discharge.nc"
+        else:
+            discharge_file = path 
         if discharge_file.is_file():
             with get_xarray_ds_from_file(path / "discharge.nc") as ds:
                 discharge_data = ds.load()
